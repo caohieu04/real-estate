040e84a1a7092d7684e3eb31bc0cd42b0ed10b5a
caohieu04 <caongochieu04@gmail.com>
2021-10-05 17:45:47 +0000

    Add .env script.sh main.go

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	dsn := os.Getenv("DBConnectionStr")
+	fmt.Print(dsn)
+
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
